d210bd9e41f1c7c0340cd601c2be5558361844b6
Justin Barker <jstnbarker@pm.me>
2023-12-11 14:20:15 -0500

    add parent class for node

---
+++ day8/Navigator.java
@@ -0,0 +1,13 @@
+import java.util.Vector;
+
+public class Navigator {
+    Vector<Node> forks;
+    String path;
+
+    Navigator(Vector<Node> forks, String path){
+        this.forks = forks;
+        this.path = path;
+    }
+
+
+}
