263d04dbfb91c960297f15d458bbc52f4b3677cd
Justin Barker <jstnbarker@pm.me>
2023-12-11 14:19:50 -0500

    load file

--- day8/Main.java
+++ day8/Main.java
@@ -4,19 +4,29 @@ import java.util.Scanner;
 import java.util.Vector;
 
 public class Main {
-    public static Vector<Node> load(File in){
+    public static Navigator load(File in){
         Scanner fis;
+        Vector<Node> forks = new Vector<>();
+        String path = "";
         try{
             fis = new Scanner(in);
+            path = fis.nextLine(); // get path
+            fis.nextLine(); // eat blank line
+            while(fis.hasNextLine()){
+                String currentLine = fis.nextLine();
+                forks.add(new Node(
+                        currentLine.substring(0,3), // id
+                        currentLine.substring(7,10), // left
+                        currentLine.substring(12,15) // right
+                ));
+            }
         } catch (FileNotFoundException e){
             System.out.println(e);
         }
-        while(fis.hasNextLine()){
-
-        }
-
+        return new Navigator(forks,path);
     }
     public static void main(String args[]){
-        Vector<Node> intersections = load(new File(args[0]));
+        Navigator nav = load(new File(args[0]));
+        return;
     }
 }
