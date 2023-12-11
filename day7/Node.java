aeb739717825c47cdc5afdbaf35a3008f532f22d
Justin Barker <jstnbarker@pm.me>
2023-12-10 19:04:19 -0500

    made node

---
+++ day7/Node.java
@@ -0,0 +1,17 @@
+public class Node {
+    private String left;
+    private String right;
+
+    public Node(String left, String right) {
+        this.left=left;
+        this.right=right;
+    }
+
+    public String getLeft() {
+        return left;
+    }
+
+    public String getRight() {
+        return right;
+    }
+}
