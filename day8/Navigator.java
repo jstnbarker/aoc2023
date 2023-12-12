8fbcfb6ccd5c571d521602a1e9a2e92568895b54
Justin Barker <jstnbarker@pm.me>
2023-12-11 21:00:53 -0500

    lcm solution

--- day8/Navigator.java
+++ day8/Navigator.java
@@ -28,40 +28,81 @@ public class Navigator {
         return pathLength;
     }
 
-    public int part2(){
-        Vector<Node> positions = new Vector<>();
+    private int length2(Node current){
+        int pathLength=0;
+        for(int i = 0; i < path.length(); i=(i+1)%path.length()){
+            if(current.getId().charAt(2)=='Z') return pathLength;
+            if(path.charAt(i)=='R'){
+                current=getNode(current.getRight());
+            }
+            else current=getNode(current.getLeft());
+            pathLength+=1;
+        }
+        return pathLength;
+    }
+    public long part2(){
+        Vector<Integer> lengths = new Vector<>();
         // add all nodes ending in A to tracked list
         for(String id : forks.keySet()){
             Node node = forks.get(id);
             if(node.getId().charAt(2)=='A'){
-                positions.add(new Node(node));
+                lengths.add(length2(node));
             }
+        }
+        return lcm(lengths);
+    }
+
+    public long lcm(Vector<Integer> x){
+        long lcm = 1;
+        int divisor=2;
+        int xSum=-1;
+        while(xSum!=x.size()) {
+            boolean isDivisible = false;
+            for (int i = 0; i != x.size(); i++) {
+                if(x.get(i)%divisor==0){
+                    isDivisible=true;
+                    x.set(i, x.get(i)/divisor);
+                }
+            }
+
+
+            if(!isDivisible) {
+                divisor = nextPrime(divisor);
+            } else {
+                lcm*=divisor;
+            }
+
+            xSum=0;
+            for(int value : x){
+                xSum+=value;
+            }
+        }
+        return lcm;
+    }
+
+    private boolean isPrime(int n)
+    {
+        if (n <= 3)  return true;
+        // This is checked so that we can skip
+        // middle five numbers in below loop
+        if (n%2 == 0 || n%3 == 0) return false;
+
+        for (int i=5; i*i<=n; i=i+6)
+            if (n%i == 0 || n%(i+2) == 0)
+                return false;
+        return true;
+    }
+
+    private int nextPrime(int N)
+    {
+        int prime = N;
+        boolean found = false;
+        while (!found) {
+            prime++;
+            if (isPrime(prime))
+                found = true;
         }
 
-        int length = 0;
-        for(int i = 0; i < path.length(); i=(i+1)%path.length()) {
-            if (path.charAt(i) == 'R') {
-                // update each node in positions with their right
-                for (int j = 0; j < positions.size(); j++) {
-                    positions.set(j, getNode(positions.get(j).getRight()));
-                }
-            } else {
-                // update each node in positions with their left
-                for (int j = 0; j < positions.size(); j++) {
-                    positions.set(j, getNode(positions.get(j).getLeft()));
-                }
-            }
-            length+=1;
-            // check last character of each node in positions
-            boolean areAllZ = true;
-            for(int j = 0; j < positions.size(); j++) {
-                if (positions.get(j).getId().charAt(2) != 'Z') {
-                    areAllZ=false;
-                    break;
-                }
-            }
-            if(areAllZ) return length;
-        }
-        return 0;
+        return prime;
     }
 }
