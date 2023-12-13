b25ec8552fde4c840a2d725ce6dcee979fcd612e
Justin Barker <jstnbarker@pm.me>
2023-12-12 20:03:02 -0500

    add load function

---
+++ day10/Main.java
@@ -0,0 +1,22 @@
+import java.io.FileNotFoundException;
+import java.util.Vector;
+import java.util.Scanner;
+import java.io.File;
+public class Main {
+    public Vector<String> load(File file){
+        Vector<String> out = new Vector<>();
+        try{
+            Scanner in = new Scanner(file);
+            while(in.hasNextLine()){
+                out.add(in.nextLine());
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println(e);
+        }
+        return out;
+    }
+
+    public static void main(String[] args){
+
+    }
+}
