402c2aeb911cc66766fe36ba7af07fe67dc73647
Justin Barker <jstnbarker@pm.me>
2023-12-13 13:17:52 -0500

    create mirrormap class and constructor

---
+++ day13/MirrorMap.java
@@ -0,0 +1,15 @@
+import java.util.Vector;
+
+public class MirrorMap {
+    Vector<String> map;
+    public MirrorMap(Vector<String> map){
+        this.map=map;
+    }
+
+    // mirror is on y axis if returned value > 100
+    // otherwise mirror is on x axis
+    public int findMirror(){
+
+
+    }
+}
